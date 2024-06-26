016ab1cae5d54045a8ccd09db2e06673a1f5cd61
Ali Aamir <111776866+ali432003@users.noreply.github.com>
2024-06-26 23:27:59 +0500

    added hosted link of netlify

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ import { app, server } from './Socket/socket.js'
 import { cloudinaryConfig } from './config/cloudinaryConfig.js';
 
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 dotenv.config();
 
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({ origin: "*" }))
+app.use(cors({
+    origin: "https://main--chatappmernbyme.netlify.app"
+}));
 
 //Cloudinary Config
 cloudinaryConfig()
@@ -27,11 +29,11 @@ cloudinaryConfig()
 app.use('/api/auth', authRouter)
 app.use('/api/message', messageRouter)
 app.use('/api/user', userRouter)
-app.use(express.static(path.join(__dirname, "./frontend/dist")))
+// app.use(express.static(path.join(__dirname, "./frontend/dist")))
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-})
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+// })
 
 const PORT = process.env.PORT || 5000
 
