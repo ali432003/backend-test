720b72227e933a52388f0e3878ac11285b43edaf
Ali Aamir <111776866+ali432003@users.noreply.github.com>
2024-06-26 23:42:50 +0500

    .

--- utils/jwtwebToken.js
+++ utils/jwtwebToken.js
@@ -1,14 +1,14 @@
 import jwt from "jsonwebtoken"
 
-const jwtToken = (userId , res)=>{
-    const token = jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:'1d'
+const jwtToken = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: '1d'
     })
-    res.cookie('jwt',token,{
+    res.cookie('jwt', token, {
         maxAge: 24 * 60 * 60 * 1000,
-        httpOnly:true,
-        sameSite:"strict",
-        secure:process.env.SECURE !== "devlopment"
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.SECURE === "production"
     })
 }
 
