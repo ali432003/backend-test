bbda037d80117517036a8fefcff58662fb50f569
Ali Aamir <111776866+ali432003@users.noreply.github.com>
2024-07-11 13:58:33 +0500

    Changes to be committed: 	modified:   utils/jwtwebToken.js

--- utils/jwtwebToken.js
+++ utils/jwtwebToken.js
@@ -7,7 +7,7 @@ const jwtToken = (userId, res) => {
     res.cookie('jwt', token, {
         maxAge: 24 * 60 * 60 * 1000, // 1 day
          // Important for security, prevents client-side JavaScript from accessing the cookie
-        secure: true, // Use secure cookies in production (HTTPS)
+        secure: process.env.NODE_ENV === "production", // Use secure cookies in production (HTTPS)
         sameSite: 'strict'
     })
 }
